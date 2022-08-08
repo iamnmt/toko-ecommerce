3106559a977a6c6f62b0fb51307f584f6a90ae08
3n0ch <iamnmt03@gmail.com>
2022-08-08 18:29:32 +0700

    simple layout for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,21 @@ const Home: NextPage = () => {
         <title>Toko | Book Store Online</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="w-[1270px] mx-12">
+        <div className="flex items-center bg-red-200">
+          <div>
+            Sidebar
+          </div>
+          <div>
+            <div>
+              Sorter
+            </div>
+            <div>
+              ProductList
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
