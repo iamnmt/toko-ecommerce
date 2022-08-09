c7a2bfa723c8a7d9077d4dc475f3ca7aa5b20b70
3n0ch <iamnmt03@gmail.com>
2022-08-09 10:40:17 +0700

    add product item

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image"
+
 function ProductItem({ info } : any) {
+  const { image, name } = info
   return (
-    <div>{info.name}</div>
+    <a className="cursor-pointer hover:shadow-2xl">
+      <div className="px-3 py-2 w-min">
+        <div className="m-3 w-[200px] h-[200px]">
+          <Image width='200' height='200' src={`/images/resources/${image}`}/>
+        </div>
+        <div className="flex flex-col">
+          <p className="text-sm break-words">{name}</p>
+        </div>
+      </div>
+    </a>
   )
 }
 
