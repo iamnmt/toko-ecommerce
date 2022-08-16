a124bcb75e9e36a74aa57ca1048146b5bcb0daec
3n0ch <iamnmt03@gmail.com>
2022-08-16 20:49:14 +0700

    fix ProductList component layout

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,7 +2,7 @@ import ProductItem from "./ProductItem"
 
 function ProductList({ productData } : any) {
   return (
-    <div className="flex flex-wrap justify-between">
+    <div className="flex flex-wrap">
       {productData.map((info : any) => <ProductItem key={info.id} info={info} />)}
     </div>
   )
