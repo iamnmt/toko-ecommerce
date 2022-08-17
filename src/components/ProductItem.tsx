bc2f6e00b403d835f5d53aa22844f64560a29b4e
3n0ch <iamnmt03@gmail.com>
2022-08-17 11:35:22 +0700

    redirect to detail page when click to book

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,26 +1,29 @@
 import Image from "next/image"
+import Link from "next/link"
 import ProductPrice from "./minor/ProductPrice"
 import RatingBar from "./minor/RatingBar"
 
 function ProductItem({ info } : any) {
-  const { title, rating, sold, price, discountPercent } = info
+  const { title, rating, sold, price, discountPercent, id } = info
   return (
-    <a className="cursor-pointer hover:shadow-2xl">
-      <div className="px-3 py-2 w-min">
-        <div className="m-3 w-[200px] h-[200px]">
-          <Image width='200' height='200' src={`/images/resources/product-preview.jpg.webp`}/>
-        </div>
-        <div className="flex flex-col">
-          <p className="text-sm break-words">{title}</p>
-          <div className="flex items-center">
-            <RatingBar activeCount={rating} size={14}/>
-            <div className="w-[2px] h-3 bg-[#c7c7c7] mx-1.5"></div>
-            <span className="text-[#787878] text-sm font-medium">{sold} Sold</span>
+    <Link href={`/detail/${id}`}>
+      <a className="cursor-pointer hover:shadow-2xl">
+        <div className="px-3 py-2 w-min">
+          <div className="m-3 w-[200px] h-[200px]">
+            <Image width='200' height='200' src={`/images/resources/product-preview.jpg.webp`}/>
           </div>
-          <ProductPrice price={price} discountPercent={discountPercent} />
+          <div className="flex flex-col">
+            <p className="text-sm break-words">{title}</p>
+            <div className="flex items-center">
+              <RatingBar activeCount={rating} size={14}/>
+              <div className="w-[2px] h-3 bg-[#c7c7c7] mx-1.5"></div>
+              <span className="text-[#787878] text-sm font-medium">{sold} Sold</span>
+            </div>
+            <ProductPrice price={price} discountPercent={discountPercent} />
+          </div>
         </div>
-      </div>
-    </a>
+      </a>
+    </Link>
   )
 }
 
